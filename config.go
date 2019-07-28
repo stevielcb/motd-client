b8c234264907b82c824b42093c9b4ce5e0cd93f2
stevenwoah <stevenwoah@users.noreply.github.com>
2019-07-27 17:47:40 -0700

    - Ran code through gofmt - Escape sequences based on TERM moved from motd-server here

--- config.go
+++ config.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-  "github.com/kelseyhightower/envconfig"
+	"github.com/kelseyhightower/envconfig"
 )
 
 var (
-  c Config
+	c Config
 )
 
 type Config struct {
-  Host      string `default:"localhost"`
-  Port      int    `default:"4200"`
-  TimeoutMs int    `default:"100" split_words:"true"`
+	Host      string `default:"localhost"`
+	Port      int    `default:"4200"`
+	TimeoutMs int    `default:"100" split_words:"true"`
 }
 
 func init() {
-  err := envconfig.Process("motd", &c)
-  if err != nil {
-    panic(err)
-  }
+	err := envconfig.Process("motd", &c)
+	if err != nil {
+		panic(err)
+	}
 }
--- main.go
+++ main.go
@@ -1,26 +1,47 @@
 package main
 
 import (
-  "bytes"
-  "fmt"
-  "io"
-  "net"
-  "os"
-  "time"
+	"bytes"
+	"fmt"
+	"io"
+	"net"
+	"os"
+	"strings"
+	"time"
+)
+
+var (
+	start string
+	end   string
 )
 
 func main() {
-  conn, err := net.DialTimeout(
-    "tcp",
-    fmt.Sprintf("%s:%d", c.Host, c.Port),
-    time.Duration(c.TimeoutMs) * time.Millisecond,
-  )
-  if err != nil {
-    os.Exit(1)
-  }
-  defer conn.Close()
+	conn, err := net.DialTimeout(
+		"tcp",
+		fmt.Sprintf("%s:%d", c.Host, c.Port),
+		time.Duration(c.TimeoutMs)*time.Millisecond,
+	)
+	if err != nil {
+		os.Exit(1)
+	}
+	defer conn.Close()
 
-  var buf bytes.Buffer
-  io.Copy(&buf, conn)
-  fmt.Print(&buf)
+	var buf bytes.Buffer
+	io.Copy(&buf, conn)
+
+	_, ok := os.LookupEnv("TERM")
+	if ok {
+		term := os.Getenv("TERM")
+		if strings.HasPrefix(term, "screen") {
+			start = "\033Ptmux;\033\033]"
+			end = "\a\033\\"
+		} else {
+			start = "\033]"
+			end = "\a"
+		}
+	} else {
+		os.Exit(1)
+	}
+
+	fmt.Printf("%s%s%s\n", start, &buf, end)
 }
